d5ff104c44043779bb77a6f4939eada9dfd10c73
lucas_home <lucasmais@dcc.ufmg.br>
2023-08-15 14:16:57 -0300

    Dictionary API finished

--- Dictionary_API/main.py
+++ Dictionary_API/main.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template
+import pandas as pd
+
 
 app = Flask(__name__)
+
+df = pd.read_csv("dictionary.csv")
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/api/v1/<word>")
 def about(word):
-    n_word = word.upper()
-    result_dictionary = {"definition": n_word,
-            "word": word}
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+    result_dictionary = {"word": word,
+                         "definition": definition}
     return result_dictionary
+
 
 if __name__ == "__main__":
     app.run(debug=True)
